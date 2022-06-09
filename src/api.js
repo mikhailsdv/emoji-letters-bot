7fb1552ddc18bad0b8da8ea10cd8ac5bc6d2a028
BaukaErenbay <mavlyd.saidov@gmail.com>
2022-06-09 18:58:02 +0600

    use local json database

---
+++ src/api.js
@@ -0,0 +1,34 @@
+const {join} = require("path")
+const {Low, JSONFile} = require("lowdb-node")
+const {fileURLToPath} = require("url")
+
+const file = join(__dirname, "db.json")
+const adapter = new JSONFile(file)
+const db = new Low(adapter)
+
+const saveRequest = async ({
+	from_id,
+	username,
+	first_name,
+	language_code,
+	text,
+	letter,
+	status,
+	mode,
+}) => {
+	await db.read()
+	db.data.requests.push({
+		from_id,
+		username,
+		first_name,
+		language_code,
+		text,
+		letter,
+		status,
+		mode,
+		dete: new Date().toUTCString(),
+	})
+	await db.write()
+}
+
+module.exports = {saveRequest}
