f5cc72a6e7c85e914f61b59ca1c3a83a08edf2ff
BaukaErenbay <mavlyd.saidov@gmail.com>
2022-06-09 15:37:53 +0600

    setting up a webhook server

--- src/index.js
+++
@@ -1,7 +0,0 @@
-require("dotenv").config()
-const env = process.env
-const bot = require("./bot.js")
-/*expressApp.use(bot.webhookCallback('/' + TOKEN));
-  bot.telegram.setWebhook(URL + TOKEN);*/
-
-bot.startWebhook(`/bot${env.BOT_TOKEN}`, null, env.PORT)
--- utils.js
+++
@@ -1,7 +0,0 @@
-const trimMessage = str => str.replace(/\t+/gm, "")
-
-const arrayRandom = arr => {
-	return arr[Math.floor(Math.random() * arr.length)]
-}
-
-export {trimMessage, arrayRandom}
